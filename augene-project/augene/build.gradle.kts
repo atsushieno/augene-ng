4e3642a45a04ec7547ad9ce80220e3a7d16556f9
atsushieno <atsushieno@gmail.com>
2021-08-23 07:06:59 +0900

    bump mugene to 0.2.16 and add gitpack repo for missing-dot.

--- augene-project/augene/build.gradle.kts
+++ augene-project/augene/build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
     mavenLocal()
     google()
     mavenCentral()
+    maven { url = uri("https://jitpack.io") }
 }
 
 plugins {
@@ -79,7 +80,7 @@ kotlin {
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
 
                 implementation("dev.atsushieno:ktmidi:0.3.8")
-                implementation("dev.atsushieno:mugene:0.2.15")
+                implementation("dev.atsushieno:mugene:0.2.16")
                 implementation("dev.atsushieno:kotractive:0.1")
             }
         }
