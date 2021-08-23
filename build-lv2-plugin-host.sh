078fd01542986167d3a367a2add0646ec283d80d
atsushieno <atsushieno@gmail.com>
2021-08-24 02:42:33 +0900

    add missing patches and build script to AudioPluginHost local build.

---
+++ build-lv2-plugin-host.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd external/tracktion_engine/modules/juce/
+patch -i ../../../../audiopluginhost-lv2.patch -p1
+cd ../../../../
+mkdir build-pluginhost
+cd build-pluginhost
+pwd
+cmake -DCMAKE_BUILD_TYPE=Debug -DJUCE_BUILD_EXTRAS=ON ../external/tracktion_engine/modules/juce/
+make
+cd ..
