bcb5c70c802e12f816400626dd15f3d8fe858562
atsushieno <atsushieno@gmail.com>
2021-09-22 03:27:45 +0900

    augene desktop standalone executable can act as a command line compiler now.

--- augene-editor-project/desktop/src/jvmMain/kotlin/Main.kt
+++ augene-editor-project/desktop/src/jvmMain/kotlin/Main.kt
@@ -1,11 +1,21 @@
 import dev.atsushieno.augene.gui.App
 import androidx.compose.ui.window.singleWindowApplication
+import dev.atsushieno.augene.AugeneModel
 import dev.atsushieno.augene.gui.SwingDialogs
 import dev.atsushieno.augene.gui.model
+import kotlin.system.exitProcess
 
-fun main() = singleWindowApplication {
-    model.dialogs = SwingDialogs(this.window)
-    model.loadConfiguration()
-    App()
-    model.processExit()
+fun main(args: Array<String>) = singleWindowApplication {
+    if (args.any()) {
+        val model = AugeneModel()
+        model.loadProjectFile(args[0])
+        model.compile()
+        println("augene compiler completed.")
+        exitProcess(0)
+    } else {
+        model.dialogs = SwingDialogs(this.window)
+        model.loadConfiguration()
+        App()
+        model.processExit()
+    }
 }
